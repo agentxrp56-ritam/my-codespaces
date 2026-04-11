d634db0274841a0fdf8d58d88420cf58e21c8813
AgentX <agentxrp56@gmail.com>
2026-04-11 07:15:27 +0000

    first commit

---
+++ web-development/simple-website/src/components/Section1/Section1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Section1 = () => {
+  return (
+    <div className='h-screen w-full'>
+      <Navbar />
+    </div>
+  )
+}
+
+
+export default Section1
